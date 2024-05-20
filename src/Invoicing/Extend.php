684e336575d0045448403bae090a3a37c5edc647
jomega <jomega@ura.go.ug>
2024-05-20 11:38:49 +0300

    handle circular reference exception

--- src/Invoicing/Extend.php
+++ src/Invoicing/Extend.php
@@ -4,7 +4,7 @@ namespace Sniper\EfrisLib\Invoicing;
 
 class Extend
 {
-    public function __construct (public string $reason,
-    public string $reasonCode) {}
+    public function __construct (public ?string $reason=null,
+    public ?string $reasonCode=null) {}
 
 }
