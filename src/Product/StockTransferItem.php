4a286bfa5fdd1a68b81cb722c5e8d5916455338e
jomega <jomega@ura.go.ug>
2024-05-30 10:26:08 +0300

    corrects interface for stock transfer

--- src/Product/StockTransferItem.php
+++ src/Product/StockTransferItem.php
@@ -7,13 +7,13 @@ use PhpParser\Node\Scalar\String_;
 class StockTransferItem
 {
     public function __construct(
-        public ?string $commodityGoodsId,
-        public ?string $goodsCode,
-        public ?string $measureUnit,
         public string  $quantity,
-        public ?string $remarks,
-        public ?string $sourceFuelTankId,
-        public ?string $destinationFuelTankId
+        public ?string $commodityGoodsId=null,
+        public ?string $goodsCode=null,
+        public ?string $measureUnit=null,
+        public ?string $remarks=null,
+        public ?string $sourceFuelTankId=null,
+        public ?string $destinationFuelTankId=null
     )
     {
     }
