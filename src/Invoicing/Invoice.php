e6a35ba8611bf481edef585d4dcf6ce4959814ab
jomega <jomega@ura.go.ug>
2024-06-25 11:48:50 +0300

    remove invoice getters

--- src/Invoicing/Invoice.php
+++ src/Invoicing/Invoice.php
@@ -22,48 +22,6 @@ class Invoice
     {
     }
 
-
-    /**
-     * @param string $legalName
-     * @param string $email
-     * @param string $buyerType
-     * @param string $tin
-     * @return $this
-     */
-    public function buyerDetails(string $legalName, string $email, string $buyerType = "1", string $tin = ""): Invoice
-    {
-        $this->buyerDetails = new BuyerDetails($legalName, $email, $buyerType, $tin);
-        return $this;
-    }
-
-    /**
-     * @param string $tin
-     * @param string $legalName
-     * @param string $emailAddress
-     * @param string $branchId
-     * @return $this
-     */
-    public function sellerDetails(string $tin, string $legalName, string $emailAddress, string $branchId): Invoice
-    {
-        $this->sellerDetails = new SellerDetails(tin: $tin, legalName: $legalName,emailAddress: $emailAddress);
-        return $this;
-    }
-
-    /**
-     * @param string $deviceNo
-     * @param string $issuedDate
-     * @param string $operator
-     * @param string $invoiceType
-     * @param string $invoiceKind
-     * @param string $dataSource
-     * @return $this
-     */
-    public function basicInformation(string $deviceNo, string $issuedDate, string $operator, string $invoiceType, string $invoiceKind, string $dataSource): Invoice
-    {
-        $this->basicInformation = new BasicInformation(deviceNo: $deviceNo,issuedDate: $issuedDate, operator: $operator);
-        return $this;
-    }
-
     public function addGoodDetails(string $item, string $itemCode, string $qty, string $unitPrice, string $total, string $unitOfMeasure, string $taxRate, string $tax,
                                    string $orderNumber, string $discountFlag, string $exciseFlag, string $goodsCategoryId): Invoice
     {
